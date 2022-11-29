ba7e3bd064f25c3e58b76d692e9ea6a5d7a5714b
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-29 14:51:54 +0600

    4 pillars of OOP

--- 1.Start/1.pillars-OOP.js
+++ 1.Start/1.pillars-OOP.js
@@ -1,4 +1,5 @@
 //  4 pillars of OOP
-// 1 - Encapsulation -> fewer  function parameters (less is best)
-// 2 - Abstraction -> Hiding object properties for simplicity
+// 1 - Encapsulation -> fewer  function parameters (less is best), group related variables and functions
+// 2 - Abstraction -> Hiding object properties for simplicity, removes impact of change
 // 3 -Inheritance -> inherit properties and methods from other objects (removes redundancy)
+// 4 . polymorphism -> poly means many, morp means form, get rid of long if else, switch statements
