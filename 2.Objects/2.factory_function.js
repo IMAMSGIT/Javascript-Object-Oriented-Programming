1e15cad4e7a79049455349635d12be0c94e54960
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-29 19:21:18 +0600

    factory function, not the new keyword one

---
+++ 2.Objects/2.factory_function.js
@@ -0,0 +1,13 @@
+//  in order to create mltiple objects without hardcoding
+// we need it when an object has at least one method/behvior
+// because if there is problem in one method, we have to fix multiple places
+
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("Draw");
+    },
+  };
+}
+const circle = createCircle(23, () => "NOo");
