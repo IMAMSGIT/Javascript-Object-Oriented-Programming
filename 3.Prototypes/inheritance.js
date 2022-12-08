c1dfb423b320366c474b6058ce3c30fe73ff0e6c
IMAMSGIT <imamhosain95@gmail.com>
2022-12-09 00:37:12 +0600

    inheritance in basic theory

---
+++ 3.Prototypes/inheritance.js
@@ -0,0 +1,4 @@
+// classical nheritance vs protypical inheritance
+// JS has no class, so only protypical inheritance
+// the class from where we inherit , callled base/super/parent class
+// the class which inherits properties , called derived/sub/child class
