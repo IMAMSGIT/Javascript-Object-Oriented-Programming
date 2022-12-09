b310f6a121a67526a07d35cae4fc753579ba9a00
IMAMSGIT <imamhosain95@gmail.com>
2022-12-09 17:15:58 +0600

    multi level inheritance

---
+++ 3.Prototypes/3.multi_level_inheritance.js
@@ -0,0 +1,8 @@
+// Object created by a given contructor will have
+// the same prototype
+// ex-- circle object created by Circle constructor function
+// will have the same prorotype from Circle constructor
+// also if we create an array
+// this array will have property from arrayBase
+// this arrayBase will havw property from objectBase
+// it is called multilevel inheritance
