c3a870c9276eb0c6059147c30b583905bda10e0b
IMAMSGIT <imamhosain95@gmail.com>
2022-12-09 15:13:02 +0600

    renamed and add child , parent relation

--- 3.Prototypes/1.inheritance.js
+++ 3.Prototypes/1.inheritance.js
@@ -2,3 +2,4 @@
 // JS has no class, so only protypical inheritance
 // the class from where we inherit , callled base/super/parent class
 // the class which inherits properties , called derived/sub/child class
+// if circle is child and shape is parent , we call circle is a shape
