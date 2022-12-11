efcfb06b9db455c25f62d2f87f432fca45964be0
IMAMSGIT <imamhosain95@gmail.com>
2022-12-12 00:47:36 +0600

    instance member & memebers of parent or prototype

--- 3.Prototypes/6.prototype_vs_instance_memberes.js
+++ 3.Prototypes/6.prototype_vs_instance_memberes.js
@@ -1,6 +1,27 @@
 function Circle(radius) {
+  // Instance members
   this.radius = radius;
-  this.draw = function () {
-    console.log("Draw");
+  this.move = function () {
+    console.log("MOVE");
+    this.draw();
   };
 }
+
+// Prototype members
+// adding the draw method to the parent
+Circle.prototype.draw = function () {
+  // this.move();
+  console.log("Draw");
+};
+
+// modifying the built in toString method
+Circle.prototype.toString = function () {
+  return "Circle with radius" + this.radius;
+};
+
+const c1 = new Circle(1);
+const c2 = new Circle(2);
+
+// *** we can call instance method sitting
+// inside a prototype members/ method more accurately
+// and vice versa
