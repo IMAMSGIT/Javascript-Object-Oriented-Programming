577475e18f07f2c651946f18cca5fe59ea754e1a
IMAMSGIT <imamhosain95@gmail.com>
2022-12-20 01:36:28 +0600

    polymorphism in action or practical

--- 4.Prototypical-Inheritance/6.polymorphism.js
+++ 4.Prototypical-Inheritance/6.polymorphism.js
@@ -25,6 +25,10 @@ Square.prototype.duplicate = function () {
   console.log("Duplicating in Square");
 };
 
+// Array of Shape Objects
 let shapes = [new Circle(), new Square()];
 
+// This is polymorphism in action
+// We can use many form of duplicate function
+// Also we don't have to provide Sqaure and Circle object separately
 for (let shape of shapes) shape.duplicate();
