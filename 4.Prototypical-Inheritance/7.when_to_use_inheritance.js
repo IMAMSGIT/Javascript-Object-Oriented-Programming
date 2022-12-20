9f7bf315da94343b01b295acff26a83a91d67f66
IMAMSGIT <imamhosain95@gmail.com>
2022-12-21 01:38:19 +0600

    things to know about inheritance

---
+++ 4.Prototypical-Inheritance/7.when_to_use_inheritance.js
@@ -0,0 +1,8 @@
+// inheritance is not the only way to code reuse
+// other thing called composition can be used
+// Example
+// Animal(eat()) - Mammal(walk()),Fish(swim())- Person,Dog,Goldfish
+// we created so many hierarchies, should be avoided because they are very fragile
+// Don't go more than one level
+// Favor composition over Inheritance
+// We can use Mixin ti achieve Composition
