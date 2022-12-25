b7745037dcf623b90c3ba2512a597c63650ba75a
IMAMSGIT <imamhosain95@gmail.com>
2022-12-26 01:52:09 +0600

    static method like built in  Math funtion

---
+++ 5.ES6-Classes/3.static_methods.js
@@ -0,0 +1,21 @@
+// Methods which are not available
+// not an instance but only in the class
+// So we can call them using dot after the classname
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  // Instance Method
+  draw() {}
+
+  //Static Method
+  static parse(str) {
+    const radius = JSON.parse(str).radius;
+    return new Circle(radius);
+  }
+}
+const cricle = Circle.parse("{ "radius": 1 }");
+console.log(cricle);
+
+// Just as do in JS built in functions Like Math
