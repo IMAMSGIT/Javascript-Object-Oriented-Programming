9b346d55679f0b3f45eb82fe3c0124d8b3d7ddc7
IMAMSGIT <imamhosain95@gmail.com>
2022-12-29 01:51:33 +0600

    tools necessary in ES6

---
+++ 6.ES6-Modules/1.ES6_tooling.js
@@ -0,0 +1,9 @@
+// important for onlty borwser application
+// Modern JS needs two kinds of tools
+// 1-Transpiler, 2-Bundler
+// 1-Transpiler means Translator+compiler
+// It's a tool we give it to our modern JS
+// and it converts to our JS code that all browser understand
+// 2-Bundler - is responsible for combining
+// all our JS files into one JS file
+// which we call Bundle ex. WEBPACK
