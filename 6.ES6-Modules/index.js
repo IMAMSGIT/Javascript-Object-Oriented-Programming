ab0607fba816de8cc8ddfac3e961ca8ada3fd9af
IMAMSGIT <imamhosain95@gmail.com>
2022-12-30 15:23:49 +0600

    how to access private property from inside

--- 6.ES6-Modules/index.js
+++ 6.ES6-Modules/index.js
@@ -1,3 +1,6 @@
 import { Circle } from "./circle.js";
 const c = new Circle(111);
+// if we didn't separate the file in module
+// we could get access the private property
+// console.log(_radius.get(c));
 c.draw();
